f487c79f017237f13746c87cc906b4547a07b99c
unusual-thoughts <henritonchain@gmail.com>
2015-06-23 19:23:40 +0200

    workaround unconv stdout bug

--- pdfconv.py
+++ pdfconv.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 import os
 from gi.repository import Gtk, GObject
-from subprocess import Popen, check_output, call, PIPE
+from subprocess import Popen, call, PIPE
 from sys import argv
 from tempfile import NamedTemporaryFile
 
-class ProgressBarWindow(Gtk.Window):
-
+class PDFConvWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Converting to PDF...")
         self.set_border_width(10)
@@ -16,7 +15,10 @@ class ProgressBarWindow(Gtk.Window):
         self.progressbar = Gtk.ProgressBar()
         vbox.pack_start(self.progressbar, True, False, 0)
         self.timeout_id = GObject.timeout_add(80, self.on_timeout)
-        self.unoconv_thread = Popen(['unoconv', '--stdout', argv[1]], stdout=tmpfile.file)
+
+        self.tmpfile = NamedTemporaryFile(suffix='.pdf', prefix='pdfconv_', dir='/tmp', delete=False)
+        self.tmpfile.file.close()
+        self.unoconv_thread = Popen(['unoconv', '-o', self.tmpfile.name, argv[1]])
     def on_timeout (self):
         self.progressbar.pulse()
         if self.unoconv_thread.poll() is not None:
@@ -24,11 +26,11 @@ class ProgressBarWindow(Gtk.Window):
             self.hide()
             Gtk.main_quit()
         return True
-tmpfile = NamedTemporaryFile(suffix='.pdf', prefix='pdfconv_', dir='/tmp') 
-win = ProgressBarWindow()
+
+win = PDFConvWindow()
 win.connect("delete-event", Gtk.main_quit)
 win.set_position(Gtk.WindowPosition.CENTER)
 win.show_all()
 Gtk.main()
-call(['evince', tmpfile.name])
-os.unlink(tmpfile.name)
+call(['evince', win.tmpfile.name])
+os.unlink(win.tmpfile.name)
